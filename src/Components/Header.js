80aa0f3d601a3a0bc34c25ddd4e16b0020648a64
vikash <vikash.tomar.india@gmail.com>
2023-07-20 15:02:11 +0530

    first commit

---
+++ src/Components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Header() {
+
+ 
+
+
+  return (
+    <div>
+      <h1>Product</h1>
+      
+
+       
+    </div>
+  )
+}
+
+export default Header
---
+++ src/Components/Product.js
@@ -0,0 +1,50 @@
+import React,{useState} from 'react'
+
+function Product(){
+
+const [cart ,setCart] = useState([])
+
+  const products = [
+    {id:1,name:"Product A",price:20},
+    {id:2,name:"Product B",price:30},
+    {id:3,name:"Product C",price:40},
+    {id:4,name:"Product D",price:50}
+]
+
+
+console.log("Hello")
+
+function handlechange(e,val) {
+    e.preventDefault()
+       setCart([...cart,val]) 
+}
+console.log(cart)
+
+
+  return (
+    <div>
+     {
+        products.map((val,index)=>{
+           
+            return (
+                
+                <div  key={index}> 
+                <p>{val.id}</p>
+                <h1>{val.name}</h1>
+                <p>{val.price}</p>
+
+                <a href="#" onClick={(e) => {handlechange(e,val)}}>Add To cart</a>   
+
+                
+                </div>
+            )
+          
+            
+            
+        })
+     } 
+    </div>
+  )
+}
+
+export default Product
