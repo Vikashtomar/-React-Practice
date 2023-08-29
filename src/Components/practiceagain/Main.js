35fe9ade802d7d9864da74d22ffd1f3de429d83f
vikash <vikash.tomar.india@gmail.com>
2023-08-30 00:25:48 +0530

    add items

--- src/Components/practiceagain/Main.js
+++ src/Components/practiceagain/Main.js
@@ -12,7 +12,9 @@ import { globalData } from './App'
     {name:"item 2",des:"this is item 2", price: 200},
     {name:"item 3",des:"this is item 3", price: 300},
     {name:"item 4",des:"this is item 4", price: 400},
-    {name:"item 5",des:"this is item 5", price: 500}
+    {name:"item 5",des:"this is item 5", price: 500},
+    {name:"item 6",des:"this is item 6", price: 600},
+
 
 
 ]
