870d898997648fb1f4a4c8f1bdbf5f6a70540257
Vikash Tomar <74128139+Vikashtomar@users.noreply.github.com>
2023-12-21 22:25:03 +0530

    Update Section.js

--- src/Components/props/Section.js
+++ src/Components/props/Section.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Footer from './Footer'
 
 function Section({name,lname}) {
-  return (
+  return ( 
     <div>
         < Footer name={name} lname = {lname}/>
     </div>
